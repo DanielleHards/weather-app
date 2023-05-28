457f24e97239acc895e1a32d7b0d37baee3f9001
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-05-28 10:44:53 -0400

    added JS file and edited html file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
-alert("Hello, Danielle");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0 ${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let conditionElement = document.querySelector("#condition");
+  conditionElement.innerHTML = response.data.condition.description;
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#day");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
+
+let apiKey = "b99atfd426b3cde797eo6c02fa816d9b";
+let city = document.querySelector("#city-input");
+city = city.value;
+let units = "imperial";
+let apiURLCurrentWeather = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=${units}`;
+console.log(apiURLCurrentWeather);
+axios.get(apiURLCurrentWeather).then(displayTemperature);
