1066a3aadd3a38097dfbc47659217d015c450299
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-06-01 21:29:24 -0400

    added JS loop to populate forecast and integrate the API

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = `0 ${minutes}`;
+    hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0 ${minutes}`;
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
@@ -19,6 +19,49 @@ function formatDate(timestamp) {
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
+}
+function formatDay(timestamp) {
+  let date = new date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+    <div class="col-2"
+        <ul class="weather-forecast-day">  
+            <li><span class="weather-forecast-date" id="day-1-name">${formatDay(
+              forecastDay.time
+            )}</span></li>
+            <li><span id="day-1-icon"><img
+          src=${forecastDay.condition.icon_url}
+          alt=""
+          width="42"
+        /></li>
+            <li> <span class="highTemp" id="high-temp">${Math.round(
+              forecastDay.temperature.maximum
+            )}°</span> <span class="lowTemp" id="low-temp"> | ${Math.round(
+          forecastDay.temperature.minimum
+        )}°</span></li>
+        </ul>
+      </div>
+      `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
 function displayTemperature(response) {
@@ -38,6 +81,14 @@ function displayTemperature(response) {
   iconElement.setAttribute("src", response.data.condition.icon_url);
   fTemp = response.data.temperature.current;
   imperialSpeed = response.data.wind.speed;
+}
+
+function getForecast(city) {
+  let apiKey = "b99atfd426b3cde797eo6c02fa816d9b";
+  let units = "imperial";
+  let apiURLForecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+  console.log(apiURLForecast);
+  axios.get(apiURLForecast).then(displayForecast);
 }
 function search(city) {
   let apiKey = "b99atfd426b3cde797eo6c02fa816d9b";
@@ -89,3 +140,4 @@ let convertF = document.querySelector("#convertF");
 convertF.addEventListener("click", convertToImperial);
 
 search("London");
+displayForecast();
